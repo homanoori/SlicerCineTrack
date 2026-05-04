9faeaf609e1a4d33af5448fc76839d607bea4ab4
homanoori <homa.ahmadinoori2004@gmail.com>
2026-05-04 16:12:17 -0400

    Make transforms file optional and handle multiple transform types

--- Track/Track.py
+++ Track/Track.py
@@ -1299,115 +1299,229 @@ class TrackWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
 
                            
     if caller == "applyTransformsButton" and event == "clicked":
-      # Set a param to hold the path to the transformations .csv file
-      self.customParamNode.transformsFilePath = self.selectorTransformsFile.currentPath
 
-      numImages = self.customParamNode.totalImages
-      # If even one line cannot be read correctly/is missing our playback cannot be successful. We
-      # will validate the tranformations input first. If the input is valid, we get a list
-      # containing all of the transformations read from the file.
-      headers = []
-      headers.append(self.columnXSelector.currentText)
-      headers.append(self.columnYSelector.currentText)
-      headers.append(self.columnZSelector.currentText)
-      transformsList = \
-        self.logic.validateTransformsInput(self.selectorTransformsFile.currentPath, numImages,headers)
+      if self.transformTypeDropdown.currentText == "Translation":
 
-      if transformsList:
-        # Create transform nodes from the transform data and place them into a sequence node
-        transformsSequenceNode = \
-           self.logic.createTransformNodesFromTransformData(shNode, transformsList, numImages)
 
-        if not transformsSequenceNode:
-          # If cancelled unset param to hold path to the transformations .csv file
-          self.customParamNode.transformsFilePath = ""
+        # Set a param to hold the path to the transformations .csv file
+
+        numImages = self.customParamNode.totalImages
+
+
+        if self.selectorTransformsFile.currentPath:
+          
+
+          # If even one line cannot be read correctly/is missing our playback cannot be successful. We
+          # will validate the tranformations input first. If the input is valid, we get a list
+          # containing all of the transformations read from the file.
+          headers = []
+          headers.append(self.columnXSelector.currentText)
+          headers.append(self.columnYSelector.currentText)
+          headers.append(self.columnZSelector.currentText)
+          transformsList = \
+            self.logic.validateTransformsInput(self.selectorTransformsFile.currentPath, numImages,headers)
+          
         else:
-          # Set a param to hold the sequence node which holds the transform nodes
-          self.customParamNode.sequenceNodeTransforms = transformsSequenceNode
-          # Create a sequence browser node
-          sequenceBrowserNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSequenceBrowserNode", \
-                                                                   "Sequence Browser")
-          sequenceBrowserNode.AddSynchronizedSequenceNode(self.customParamNode.sequenceNode2DImages)
-          sequenceBrowserNode.AddSynchronizedSequenceNode(self.customParamNode.sequenceNodeTransforms)
-          # We need to observe the changes to the sequence browser so that our GUI will update as
-          # the sequence progresses
-          self.addObserver(sequenceBrowserNode, vtk.vtkCommand.ModifiedEvent, \
-                           self.updateGUIFromParameterNode)
-          # Set a param to hold the sequence browser node
-          self.customParamNode.sequenceBrowserNode = sequenceBrowserNode
-          
-          # Since the code above added another set of image nodes, transforms nodes and
-          # sequence browser nodes, remove the unused sequence browser node, image nodes,
-          # and transforms nodes, if they exist
-          nodes = slicer.mrmlScene.GetNodesByClass("vtkMRMLSequenceBrowserNode")
-          nodes.UnRegister(None)
-          # Ensure that there is an extra sequence browser node, since we need exactly
-          # one sequence browser node at a time
-          if nodes.GetNumberOfItems() == 2:
-            sequenceBrowserNodeToDelete = nodes.GetItemAsObject(0)
-              
-            # Remove the unused sequence browser node
-            slicer.mrmlScene.RemoveNode(sequenceBrowserNodeToDelete)
+          # No file provided — use identity transform (0,0,0) for each frame
+          self.customParamNode.transformsFilePath = ""
+          transformsList = [[0.0, 0.0, 0.0] for _ in range(numImages)]
 
-            # Remove the unused Transforms Nodes Sequence, if it exists
-            nodes = slicer.mrmlScene.GetNodesByClassByName("vtkMRMLSequenceNode", "Transform Nodes Sequence")
-            nodes.UnRegister(None)
-            if nodes.GetNumberOfItems() == 2:
-              nodeToRemove = nodes.GetItemAsObject(0)
-              slicer.mrmlScene.RemoveNode(nodeToRemove)
+        if transformsList:
+          # Create transform nodes from the transform data and place them into a sequence node
+          transformsSequenceNode = \
+            self.logic.createTransformNodesFromTransformData(shNode, transformsList, numImages)
+
+          if not transformsSequenceNode:
+            # If cancelled unset param to hold path to the transformations .csv file
+            self.customParamNode.transformsFilePath = ""
+          else:
+            # Set a param to hold the sequence node which holds the transform nodes
+            self.customParamNode.sequenceNodeTransforms = transformsSequenceNode
+            # Create a sequence browser node
+            sequenceBrowserNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSequenceBrowserNode", \
+                                                                    "Sequence Browser")
+            sequenceBrowserNode.AddSynchronizedSequenceNode(self.customParamNode.sequenceNode2DImages)
+            sequenceBrowserNode.AddSynchronizedSequenceNode(self.customParamNode.sequenceNodeTransforms)
+            # We need to observe the changes to the sequence browser so that our GUI will update as
+            # the sequence progresses
+            self.addObserver(sequenceBrowserNode, vtk.vtkCommand.ModifiedEvent, \
+                            self.updateGUIFromParameterNode)
+            # Set a param to hold the sequence browser node
+            self.customParamNode.sequenceBrowserNode = sequenceBrowserNode
             
-            # Remove the unused Transforms Nodes Sequence containing each linear transform node, if it exists
-            nodes = slicer.mrmlScene.GetNodesByClassByName("vtkMRMLLinearTransformNode", "Transform Nodes Sequence")
+            # Since the code above added another set of image nodes, transforms nodes and
+            # sequence browser nodes, remove the unused sequence browser node, image nodes,
+            # and transforms nodes, if they exist
+            nodes = slicer.mrmlScene.GetNodesByClass("vtkMRMLSequenceBrowserNode")
             nodes.UnRegister(None)
+            # Ensure that there is an extra sequence browser node, since we need exactly
+            # one sequence browser node at a time
             if nodes.GetNumberOfItems() == 2:
-              nodeToRemove = nodes.GetItemAsObject(0)
-              slicer.mrmlScene.RemoveNode(nodeToRemove.GetStorageNode())
-              slicer.mrmlScene.RemoveNode(nodeToRemove)
-          
-            # Remove the unused Image Nodes Sequence, containing each image node, if it exists
-            nodes = slicer.mrmlScene.GetNodesByClassByName("vtkMRMLScalarVolumeNode", "Image Nodes Sequence")
-            nodes.UnRegister(None)
-            if nodes.GetNumberOfItems() == 2:
-              nodeToRemove = nodes.GetItemAsObject(0)
-              slicer.mrmlScene.RemoveNode(nodeToRemove)
+              sequenceBrowserNodeToDelete = nodes.GetItemAsObject(0)
+                
+              # Remove the unused sequence browser node
+              slicer.mrmlScene.RemoveNode(sequenceBrowserNodeToDelete)
+
+              # Remove the unused Transforms Nodes Sequence, if it exists
+              nodes = slicer.mrmlScene.GetNodesByClassByName("vtkMRMLSequenceNode", "Transform Nodes Sequence")
+              nodes.UnRegister(None)
+              if nodes.GetNumberOfItems() == 2:
+                nodeToRemove = nodes.GetItemAsObject(0)
+                slicer.mrmlScene.RemoveNode(nodeToRemove)
               
-            # Remove the image nodes of each slice view used to preserve the slice views
-            nodes = slicer.mrmlScene.GetNodesByClass("vtkMRMLScalarVolumeNode")
-            nodes.UnRegister(None)
-            for node in nodes:
-              if node.GetName() == 'Image Nodes Sequence':
-                 break
-              if node.GetName() == node.GetAttribute('Sequences.BaseName'):
-                slicer.mrmlScene.RemoveNode(node.GetDisplayNode())
-                slicer.mrmlScene.RemoveNode(node)
-          self.overlayThicknessSlider.enabled = True
+              # Remove the unused Transforms Nodes Sequence containing each linear transform node, if it exists
+              nodes = slicer.mrmlScene.GetNodesByClassByName("vtkMRMLLinearTransformNode", "Transform Nodes Sequence")
+              nodes.UnRegister(None)
+              if nodes.GetNumberOfItems() == 2:
+                nodeToRemove = nodes.GetItemAsObject(0)
+                slicer.mrmlScene.RemoveNode(nodeToRemove.GetStorageNode())
+                slicer.mrmlScene.RemoveNode(nodeToRemove)
+            
+              # Remove the unused Image Nodes Sequence, containing each image node, if it exists
+              nodes = slicer.mrmlScene.GetNodesByClassByName("vtkMRMLScalarVolumeNode", "Image Nodes Sequence")
+              nodes.UnRegister(None)
+              if nodes.GetNumberOfItems() == 2:
+                nodeToRemove = nodes.GetItemAsObject(0)
+                slicer.mrmlScene.RemoveNode(nodeToRemove)
+                
+              # Remove the image nodes of each slice view used to preserve the slice views
+              nodes = slicer.mrmlScene.GetNodesByClass("vtkMRMLScalarVolumeNode")
+              nodes.UnRegister(None)
+              for node in nodes:
+                if node.GetName() == 'Image Nodes Sequence':
+                  break
+                if node.GetName() == node.GetAttribute('Sequences.BaseName'):
+                  slicer.mrmlScene.RemoveNode(node.GetDisplayNode())
+                  slicer.mrmlScene.RemoveNode(node)
+            self.overlayThicknessSlider.enabled = True
 
-          # Load first image of the sequence when all required inputs are satisfied
-          self.resetVisuals()
+            # Load first image of the sequence when all required inputs are satisfied
+            self.resetVisuals()
+            
+        else:
+          # If the user inputted file in the Tranforms File input is not accepted, remove the nodes created
+          # from the previously inputted transforms file, if it exists. Also, remove filepath in Transforms
+          # File in the `Inputs` section since the input is invalid.
+
+          # Remove the unused Transforms Nodes Sequence, if it exists
+          nodes = slicer.mrmlScene.GetNodesByClassByName("vtkMRMLSequenceNode", "Transform Nodes Sequence")
+          nodes.UnRegister(None)
+          if nodes.GetNumberOfItems() == 1:
+            nodeToRemove = nodes.GetItemAsObject(0)
+            slicer.mrmlScene.RemoveNode(nodeToRemove)
           
+          # Remove the unused Transforms Nodes Sequence containing each linear transform node, if it exists
+          nodes = slicer.mrmlScene.GetNodesByClassByName("vtkMRMLLinearTransformNode", "Transform Nodes Sequence")
+          nodes.UnRegister(None)
+          if nodes.GetNumberOfItems() == 1:
+            nodeToRemove = nodes.GetItemAsObject(0)
+            slicer.mrmlScene.RemoveNode(nodeToRemove.GetStorageNode())
+            slicer.mrmlScene.RemoveNode(nodeToRemove)
+
+          # Remove filepath for the Transforms File in the `Inputs` section
+          self.customParamNode.transformsFilePath = ''
+          self.selectorTransformsFile.currentPath = ''
+
+      #Deformation Field
       else:
-        # If the user inputted file in the Tranforms File input is not accepted, remove the nodes created
-        # from the previously inputted transforms file, if it exists. Also, remove filepath in Transforms
-        # File in the `Inputs` section since the input is invalid.
+        if len(self.deformationFieldPaths) != self.customParamNode.totalImages:
+            slicer.util.errorDisplay("Number of deformation field files must match number of cine images.", "Input Error")
+            return
 
-        # Remove the unused Transforms Nodes Sequence, if it exists
-        nodes = slicer.mrmlScene.GetNodesByClassByName("vtkMRMLSequenceNode", "Transform Nodes Sequence")
-        nodes.UnRegister(None)
-        if nodes.GetNumberOfItems() == 1:
-          nodeToRemove = nodes.GetItemAsObject(0)
-          slicer.mrmlScene.RemoveNode(nodeToRemove)
-        
-        # Remove the unused Transforms Nodes Sequence containing each linear transform node, if it exists
-        nodes = slicer.mrmlScene.GetNodesByClassByName("vtkMRMLLinearTransformNode", "Transform Nodes Sequence")
-        nodes.UnRegister(None)
-        if nodes.GetNumberOfItems() == 1:
-          nodeToRemove = nodes.GetItemAsObject(0)
-          slicer.mrmlScene.RemoveNode(nodeToRemove.GetStorageNode())
-          slicer.mrmlScene.RemoveNode(nodeToRemove)
+        # Create the SequenceNode to store the masks
+        deformedMaskSequenceNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSequenceNode", "Deformed Mask Sequence")
+        self.customParamNode.deformedMaskSequenceNode = deformedMaskSequenceNode
 
-        # Remove filepath for the Transforms File in the `Inputs` section
-        self.customParamNode.transformsFilePath = ''
-        self.selectorTransformsFile.currentPath = ''
+        mask = sitk.ReadImage(self.customParamNode.path3DSegmentation)
+
+        for i, path in enumerate(self.deformationFieldPaths):
+            try:
+                print(f"Frame {i} — Reading transform from: {path}")
+                tx = sitk.ReadTransform(path)
+
+                toDisplacementFilter = sitk.TransformToDisplacementFieldFilter()
+                toDisplacementFilter.SetReferenceImage(mask)
+                displacementField = toDisplacementFilter.Execute(tx)
+
+                tx = sitk.DisplacementFieldTransform(displacementField)
+                deformedMask = sitk.Resample(mask, mask, tx)
+
+                print(f"DeformedMask[{i}] unique values:", np.unique(sitk.GetArrayFromImage(deformedMask)))
+
+                # Create volume node directly in memory without saving to disk
+                volumeNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLLabelMapVolumeNode", f"DeformedMask_{i}")
+                
+                # Convert SimpleITK image to numpy array and update the volume node
+                deformedMaskArray = sitk.GetArrayFromImage(deformedMask)
+                slicer.util.updateVolumeFromArray(volumeNode, deformedMaskArray)
+                
+                # Copy the image properties from the original mask
+                volumeNode.SetOrigin(mask.GetOrigin())
+                volumeNode.SetSpacing(mask.GetSpacing())
+                
+                # Set the image direction
+                direction = mask.GetDirection()
+                vtkMatrix = vtk.vtkMatrix4x4()
+                for row in range(3):
+                    for col in range(3):
+                        vtkMatrix.SetElement(row, col, direction[row * 3 + col])
+                volumeNode.SetIJKToRASDirectionMatrix(vtkMatrix)
+                
+                deformedMaskSequenceNode.SetDataNodeAtValue(volumeNode, str(i))
+
+            except Exception as e:
+                slicer.util.errorDisplay(f"Failed to apply deformation field to mask {i}: {e}")
+                return
+
+        # Playback setup
+        sequenceBrowserNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSequenceBrowserNode", "Sequence Browser")
+        sequenceBrowserNode.AddSynchronizedSequenceNode(self.customParamNode.sequenceNode2DImages)
+        sequenceBrowserNode.AddSynchronizedSequenceNode(deformedMaskSequenceNode)
+
+        sequenceBrowserNode.SetRecording(deformedMaskSequenceNode, False)
+        sequenceBrowserNode.SetPlayback(deformedMaskSequenceNode, True)
+        sequenceBrowserNode.SetSelectedItemNumber(0)
+        sequenceBrowserNode.SetPlaybackRateFps(10)
+
+        layoutManager = slicer.app.layoutManager()
+        for name in layoutManager.sliceViewNames():
+            sliceWidget = layoutManager.sliceWidget(name)
+            sliceCompositeNode = sliceWidget.mrmlSliceCompositeNode()
+
+            currentItemIndex = sequenceBrowserNode.GetSelectedItemNumber()
+            currentLabelNode = deformedMaskSequenceNode.GetDataNodeAtValue(str(currentItemIndex))
+
+            if currentLabelNode:
+                sliceCompositeNode.SetLabelVolumeID(currentLabelNode.GetID())
+                sliceCompositeNode.SetLabelOpacity(self.customParamNode.opacity)
+                sliceWidget.mrmlSliceNode().SetUseLabelOutline(self.customParamNode.overlayAsOutline)
+
+        self.addObserver(sequenceBrowserNode, vtk.vtkCommand.ModifiedEvent, self.updateGUIFromParameterNode)
+
+        # Register sequence browser (for playback control in the UI)
+        self.customParamNode.sequenceBrowserNode = sequenceBrowserNode
+
+        # Done—reset visuals to show new playback
+        self.resetVisuals()
+
+        self.updateGUIFromParameterNode()
+
+        self.logic.visualize(
+    sequenceBrowser=self.customParamNode.sequenceBrowserNode,
+    sequenceNode2DImages=self.customParamNode.sequenceNode2DImages,
+    segmentationLabelMapID=self.customParamNode.node3DSegmentationLabelMap,
+    sequenceNodeTransforms=self.customParamNode.sequenceNodeTransforms,  # This is still required by the function signature
+    opacity=self.customParamNode.opacity,
+    overlayAsOutline=self.customParamNode.overlayAsOutline,
+    overlayThickness=self.customParamNode.overlayThickness,
+    show=False,
+    customParamNode=self.customParamNode,
+    deformedMaskSequenceNode=self.customParamNode.deformedMaskSequenceNode,
+    transformType="Deformation Field"
+)
+
+
+
     self.customParamNode.EndModify(wasModified)
   def onTransformsFilePathChange(self):
     
